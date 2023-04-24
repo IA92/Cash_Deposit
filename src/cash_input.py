ead2eafbad780d0a8c6e7beb05230381a0c72589
IA92 <30815233+IA92@users.noreply.github.com>
2023-04-24 12:53:51 +1000

    Store data in dictionary. Add 'Complete' button

--- src/cash_input.py
+++ src/cash_input.py
@@ -24,7 +24,7 @@ def update_value(amount_vars, cash_list, product_vars, message_text_var):
                 # Calculate the product
                 product = value1 * value2
                 # Add it to the total_amount
-                total_amount +=product
+                total_amount += round(product, 2)
                 # Update the StringVar associated with the Label widget
                 product_vars[cash_amount].set("$"+str("{:.2f}".format(product)))
             else:
@@ -34,12 +34,14 @@ def update_value(amount_vars, cash_list, product_vars, message_text_var):
             # If the values are not valid integers, clear the result
             amount_vars[cash_amount].set("")
     # Set the text to display total amount
-    message_text_var.set(f"Total amount is ${total_amount}")
+    if (message_text_var):
+        message_text_var.set(f"Total amount is ${total_amount}")
 
 class BankDepositGUI:
     def __init__(self, master):
         self.master = master
         master.title("Bank Deposit")
+        self.deposit_data = {}
 
         self.cash_list = ["100", "50", "20", "10", "5", "2", "1", "0.50", "0.20", "0.10", "0.05"]
 
@@ -53,6 +55,7 @@ class BankDepositGUI:
         self.date_label = tk.Label(master, text="Select date:")
         self.date_entry = DateEntry(master, locale= "en_AU", width=12, background='darkblue', foreground='white', borderwidth=2)
         self.deposit_button = tk.Button(master, text="Deposit", command=self.deposit)
+        self.complete_button = tk.Button(master, text="Complete", command=self.generate_report)
 
         # Grid widgets
         self.date_label.grid(row=0, column=0, padx=5, pady=5, sticky='w')
@@ -72,34 +75,31 @@ class BankDepositGUI:
         self.message_text_var = StringVar()
         self.message_text=tk.Label(master, textvariable=self.message_text_var).grid(row=row_idx, column=0, columnspan = 4, padx=5, pady=5)
         self.deposit_button.grid(row=row_idx+1, column=1, padx=5, pady=5)
+        self.complete_button.grid(row=row_idx+2, column=1, padx=5, pady=5)
 
         # Use arrow key to navigate the entry
         self.master.bind('<Up>', next_widget)
         self.master.bind('<Down>', next_widget)
         self.master.bind('<Return>', next_widget)
+        self.master.bind("<plus>", self.deposit)
 
-    def deposit(self):
+    def deposit(self, event=None):
         # Get the date from the user inputs
         date = self.date_entry.get_date().strftime("%A, %m/%d/%Y")
 
-        # Validate user inputs
-        deposit_data = []
+        # Save user inputs
+        self.deposit_data[date] = {}
         total_daily_amount = 0
         for cash_amount in self.cash_list:
             amount = round(float(cash_amount),2)
             n_amount = int(self.amount_entry[cash_amount].get()) if self.amount_entry[cash_amount].get() else 0
             each_amount = amount*n_amount
             total_daily_amount += each_amount
-            # Create a list with the deposit data
-            if n_amount > 0:
-                deposit_data.append([date, f"${cash_amount}", f"{n_amount}", f"${each_amount}"]) if len(deposit_data)==0 else deposit_data.append(["", f"${cash_amount}", f"{n_amount}",f"${each_amount}"])
-        deposit_data.append(["Total","","",f"${total_daily_amount}"])
-
-        # Write the data to a CSV file
-        with open('deposits.csv', 'a', newline='') as csvfile:
-            writer = csv.writer(csvfile)
-            for deposit_row in deposit_data:
-                writer.writerow(deposit_row) 
+            # Save the data for each date
+            self.deposit_data[date][cash_amount]={}
+            self.deposit_data[date][cash_amount]["n_amount"]=n_amount
+            self.deposit_data[date][cash_amount]["each_amount"]=each_amount
+        self.deposit_data[date]["total_daily_amount"]=total_daily_amount
 
         # Show a confirmation message with the total amount deposited
         self.message_text_var.set(f"Deposit Successful, Daily amount deposited: ${total_daily_amount:.2f}")
@@ -115,6 +115,50 @@ class BankDepositGUI:
         self.date_entry.set_date(self.date_entry.get_date()+timedelta(days=1))
         # Clear the product var values
         update_value(self.amount_var, self.cash_list, self.product_var, NULL) #Don't update the message var
+
+    def generate_report(self, event=None):
+        # Check if there is any entry
+        if any([self.amount_entry[cash_amount].get() for cash_amount in self.cash_list]):
+            self.deposit()
+
+        # Get the deposit data and write it in an excel file
+        report = []
+        total_amount = 0
+        for date in sorted(self.deposit_data.keys(), reverse = False):
+            daily_report = []
+            for cash_amount in self.cash_list:
+                n_amount = self.deposit_data[date][cash_amount]["n_amount"]
+                each_amount = self.deposit_data[date][cash_amount]["each_amount"]
+                # Create a list with the deposit data
+                if n_amount > 0:
+                    daily_report.append([date, f"${cash_amount}", f"{n_amount}", f"${each_amount}"]) if len(daily_report)==0 else daily_report.append(["", f"${cash_amount}", f"{n_amount}",f"${each_amount}"])
+            report.extend(daily_report)
+            total_daily_amount = self.deposit_data[date]["total_daily_amount"]
+            report.append(["Total","","",f"${total_daily_amount}"])
+            total_amount += total_daily_amount
+
+        # Write the data to a CSV file
+        with open('deposits.csv', 'a', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            for deposit_row in report:
+                writer.writerow(deposit_row) 
+
+        # Show a confirmation message with the total amount deposited
+        self.message_text_var.set(f"Report Generated Successful, Total amount deposited: ${total_amount:.2f}")
+        
+        # Clear entry fields
+        for cash_amount in self.cash_list:
+            self.amount_var[cash_amount].set("")
+
+        # Reset the cursor to the top entry
+        self.amount_entry[self.cash_list[0]].focus_set()
+
+        # Clear the product var values
+        update_value(self.amount_var, self.cash_list, self.product_var, NULL) #Don't update the message var
+
+        # Disable the buttons
+        self.deposit_button.config(state="disabled")
+        self.complete_button.config(state="disabled")
 
 root = tk.Tk()
 bank_deposit_gui = BankDepositGUI(root)
